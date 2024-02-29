5d8e6f16c55b98672afae2d881673d2730bdeef3
Yadier01 <yadierg7@gmail.com>
2024-02-29 13:56:23 -0400

    formatting

--- util/random.go
+++ util/random.go
@@ -6,24 +6,26 @@ import (
 	"time"
 )
 
-const alphabet = "abcdefghijklmnopqrstuvwxyz" 
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
 func init() {
 	rand.New(rand.NewSource(time.Now().UnixNano()))
 }
-//randomInt 
+
+// randomInt
 func RandomInt(min, max int64) int64 {
 	return min + rand.Int63n(max-min+1)
 }
 
 func RandomString(n int) string {
- 	var sb strings.Builder
- k := len(alphabet)
+	var sb strings.Builder
+	k := len(alphabet)
 
- for i := 0; i < n; i++ {
-	c := alphabet[rand.Intn(k)]
-	sb.WriteByte(c)
- }
-return sb.String()
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+	return sb.String()
 }
 
 func RandomOwner() string {
