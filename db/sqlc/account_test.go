ed0285d5cde7ee7cd4071e9e5f6143e3b98b0927
Yadier01 <yadierg7@gmail.com>
2024-02-29 13:55:43 -0400

    using random util in test

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -4,14 +4,15 @@ import (
 	"context"
 	"testing"
 
+	"github.com/Yadier01/simplebank/util"
 	"github.com/stretchr/testify/require"
 )
 
 func TestCreateAcount(t *testing.T){
 	arg := CreateAccountParams{
-		Owner: "Tom",
-		Balance: 100,
-		Currency: "USD",
+		Owner: util.RandomOwner(),
+		Balance: util.RandomMoney(),
+		Currency: util.RandomCurrency(),
 	}
 
 	account, err := testQueries.CreateAccount(context.Background(), arg);
