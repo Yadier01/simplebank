434abcfc872a8f9caf6fbfce47d37cff068fae53
Yadier01 <yadierg7@gmail.com>
2024-03-04 14:17:43 -0400

    created validator

--- api/validator.go
+++ api/validator.go
@@ -1,1 +1,13 @@
 package api
+
+import (
+	"github.com/Yadier01/simplebank/util"
+	"github.com/go-playground/validator/v10"
+)
+
+var validCurency validator.Func = func(fieldLevel validator.FieldLevel) bool {
+	if curreny, ok := fieldLevel.Field().Interface().(string); ok {
+		return util.IsSupportedCurrency(curreny)
+	}
+	return false
+}
